77eac6664e17216d3f4bcc22eb9081019a59ebed
Olli Vanhoja <olli.vanhoja@gmail.com>
2024-04-12 18:34:34 +0200

    CRIs can be rounded

--- pages/cri.tsx
+++ pages/cri.tsx
@@ -12,9 +12,9 @@ export default function Text() {
 	const cri = useMemo(() => calcCRI(meas), [meas]);
 	const rows = [
 		{ id: 1, name: 'CCT', value: Math.round(meas.CCT), unit: 'K' },
-		{ id: 9, name: 'Ra', value: cri.R[0], unit: null },
-		{ id: 10, name: 'R1', value: cri.R[1], unit: null },
-		{ id: 11, name: 'R2', value: cri.R[2], unit: null },
+		{ id: 9, name: 'Ra', value: Math.round(cri.R[0]), unit: null },
+		{ id: 10, name: 'R1', value: Math.round(cri.R[1]), unit: null },
+		{ id: 11, name: 'R2', value: Math.round(cri.R[2]), unit: null },
 		{ id: 12, name: 'cs', value: cri.cs, unit: null },
 	];
 
