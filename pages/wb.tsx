b0894a8eb63b4b6a1618a8028bb226e440377066
Olli Vanhoja <olli.vanhoja@gmail.com>
2024-04-19 00:24:29 +0200

    Better WB page

--- pages/wb.tsx
+++ pages/wb.tsx
@@ -1,27 +1,59 @@
 import { useMemo } from 'react';
 import Box from '@mui/system/Box';
+import Paper from '@mui/material/Paper';
+import InputAdornment from '@mui/material/InputAdornment';
+import TextField from '@mui/material/TextField';
 import Container from '@mui/material/Container';
 import MyHead from '../components/MyHead';
 import Title from '../components/Title';
-import DataArray from '../components/DataArray';
 import { useGlobalState } from '../lib/global';
 import { calcCRI } from '../lib/cri';
 
 export default function Text() {
 	const [meas] = useGlobalState('res_lm_measurement');
 	const cri = useMemo(() => calcCRI(meas), [meas]);
-	const rows = [
-		{ id: 1, name: 'CCT', value: Math.round(meas.CCT), unit: 'K' },
-		{ id: 2, name: 'Duv', value: meas.Duv, unit: null },
-		{ id: 3, name: 'Tint', value: Math.round(meas.tint), unit: null },
-	];
+	// "The concept of correlated color temperature should not be used if the
+	// chromaticity of the test source differs more than Δuv = 5×10-2 from the
+	// Planckian radiator."
+	// Schanda, János (2007). "3: CIE Colorimetry".
+	// Colorimetry: Understanding the CIE System.
+	const duvErr = Math.abs(meas.Duv) > 5e-2;
 
 	return (
 		<Container maxWidth="md">
 			<MyHead />
 			<Box position="relative" sx={{ flexGrow: 1 }}>
 				<Title>OLM - WB</Title>
-				<DataArray rows={rows} pageSize={9} />
+				<Paper sx={{ padding: 2 }}>
+					<Box>
+						<TextField
+							label="CCT"
+							disabled
+							sx={{ m: 1, width: '15ch' }}
+							value={`${Math.round(meas.CCT)}`}
+							InputProps={{
+								endAdornment: <InputAdornment position="end">K</InputAdornment>,
+							}}
+						/>
+					</Box>
+					<Box>
+						<TextField
+							label="Duv"
+							disabled
+							sx={{ m: 1, width: '15ch' }}
+							variant="outlined"
+							error={duvErr}
+							value={`${meas.Duv.toFixed(3)}`}
+							helperText={ duvErr && "Invalid CCT?" }
+						/>
+						<TextField
+							label="Tint"
+							disabled
+							sx={{ m: 1, width: '15ch' }}
+							value={`${Math.round(meas.tint)}`}
+						/>
+					</Box>
+				</Paper>
 			</Box>
 		</Container>
 	);
