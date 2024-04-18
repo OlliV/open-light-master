01f2a1701f706e051ebff628d831b0d2cbdfd74f
Olli Vanhoja <olli.vanhoja@gmail.com>
2024-04-19 00:39:15 +0200

    Better CRI page

--- pages/cri.tsx
+++ pages/cri.tsx
@@ -1,9 +1,11 @@
 import { useMemo } from 'react';
 import Box from '@mui/system/Box';
 import Container from '@mui/material/Container';
+import InputAdornment from '@mui/material/InputAdornment';
 import MyHead from '../components/MyHead';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
 import Title from '../components/Title';
-import DataArray from '../components/DataArray';
 import { useGlobalState } from '../lib/global';
 import { calcCRI } from '../lib/cri';
 
@@ -15,13 +17,50 @@ export default function Text() {
 		{ id: 9, name: 'Ra', value: Math.round(cri.R[0]), unit: null },
 		{ id: 12, name: 'cs', value: cri.cs, unit: null },
 	];
+	// See wb.tsx
+	const duvErr = Math.abs(meas.Duv) > 5e-2;
 
 	return (
 		<Container maxWidth="md">
 			<MyHead />
 			<Box position="relative" sx={{ flexGrow: 1 }}>
 				<Title>OLM - CRI</Title>
-				<DataArray rows={rows} pageSize={9} />
+				<Paper sx={{ padding: 2 }}>
+					<Box>
+						<TextField
+							label="CCT"
+							disabled
+							sx={{ m: 1, width: '15ch' }}
+							value={`${Math.round(meas.CCT)}`}
+							InputProps={{
+								endAdornment: <InputAdornment position="end">K</InputAdornment>,
+							}}
+						/>
+						<TextField
+							label="Duv"
+							disabled
+							sx={{ m: 1, width: '15ch' }}
+							variant="outlined"
+							error={duvErr}
+							value={`${meas.Duv.toFixed(3)}`}
+							helperText={ duvErr && "Invalid CCT?" }
+						/>
+					</Box>
+					<Box>
+						<TextField
+							label="Ra"
+							disabled
+							sx={{ m: 1, width: '15ch' }}
+							value={`${Math.round(cri.R[0])}`}
+						/>
+						<TextField
+							label="cs"
+							disabled
+							sx={{ m: 1, width: '15ch' }}
+							value={`${cri.cs.toFixed(6)}`}
+						/>
+					</Box>
+				</Paper>
 			</Box>
 		</Container>
 	);
