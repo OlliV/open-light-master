f0f94efaf6f946e4e89b9ec3e5dc67c92e7588d1
Olli Vanhoja <olli.vanhoja@gmail.com>
2024-04-24 20:42:47 +0200

    Disabple autoPlay

--- pages/spectrum.tsx
+++ pages/spectrum.tsx
@@ -108,7 +108,7 @@ export default function Text() {
 							}}
 						/>
 					</Box>
-					<Carousel>
+					<Carousel autoPlay={false}>
 						<SpectrumBar data={data} />
 						<Scatter
 							data={{
